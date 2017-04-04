e96ae3866f80f11bb8d68d3c9e26d8d4b070b9cb
João Luccas Damiani <jlucasd01@gmail.com>
2017-04-03 23:32:48 -0300

    ajustes nos arquivos de servidor para o heroku

--- simplemooc/wsgi.py
+++ simplemooc/wsgi.py
@@ -10,9 +10,9 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWHiteNoise
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "simplemooc.settings")
 
 application = get_wsgi_application()
-application = DjangoWHiteNoise(application)
+application = DjangoWhiteNoise(application)
