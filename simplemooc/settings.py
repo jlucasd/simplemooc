cfa2178ca55be79e8bcbc735309ba58382830cb3
João Luccas Damiani <jlucasd01@gmail.com>
2017-04-04 20:04:59 -0300

    Alterações para publicação no heroku

--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -74,18 +74,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'simplemooc.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 
@@ -149,7 +137,9 @@ AUTH_USER_MODEL = 'accounts.User'
 
 # Heroku settings
 import dj_database_url
-DATABASES['default'] = dj_database_url.config()
+DATABASES = {
+    'default': dj_database_url.config()
+}
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDER_PROTO', 'https')
 
