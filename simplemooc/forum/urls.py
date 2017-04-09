4bed673a9cf95d02cc38a8cce94ee03dffdbc213
João Luccas Damiani <jlucasd01@gmail.com>
2017-04-09 13:51:36 -0300

    Atuaizações para aplicação e para o heroku

--- simplemooc/forum/urls.py
+++ simplemooc/forum/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^tag/(?P<tag>[\w_-]+)/$', views.index, name='index_tagged')
+    url(r'^tag/(?P<tag>[\w_-]+)/$', views.index, name='index_tagged'),
+    url(r'^respostas/(?P<pk>[\w_-]+)/correta/$', views.reply_correct, name='reply_correct'),
+    url(r'^respostas/(?P<pk>[\w_-]+)/incorreta/$', views.reply_incorrect, name='reply_incorrect'),
+    url(r'^(?P<slug>[\w_-]+)/$', views.thread, name='thread'),
 ]
